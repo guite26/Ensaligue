7d9690a12f083f2709dfd748bdd41e271a10147d
fwallyn <wallynfrancois@gmail.com>
2023-01-17 12:12:33 +0100

    création classe fille contrat pour stagiaire

--- classes/contract.py
+++ classes/contract.py
@@ -1,11 +1,19 @@
 from player import Player
 from team import Team
-from datetime import date
-
-class Contract :
-    def __init__(self,player : Player,team : Team,date_start : date,date_end : date,salary : int) -> None:
+from datetime import date, datetime
+from abc import ABC, abstractmethod
+class Contract(ABC) :
+    def __init__(self,player : Player,team : Team,date_start : date,duration:int=None,salary:int=None) -> None:
         self.player = player
         self.team = team
         self.date_start = date_start
-        self.date_end = date_end
+        self.duration = duration
         self.salary = salary
+
+    @property
+    def salary(self):
+        return self.salary
+
+    @property
+    def end_date(self):
+        return self.end_date
--- classes/player.py
+++ classes/player.py
@@ -1,6 +1,7 @@
+from datetime import date
 class Player :
-    def __init__(self,name : str,surname : str,age : int,positon : str) -> None:
+    def __init__(self,name : str,surname : str,birth_date:date,positon : str) -> None:
         self.name = name
         self.surname = surname
-        self.age = age
+        self.birth_date = birth_date
         self.position = positon
