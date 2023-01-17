b703c5b60992b2d2e1e879dbd698670a67d62996
fwallyn <wallynfrancois@gmail.com>
2023-01-17 15:31:19 +0100

    début création bdd sur postgres guillaume

--- server/database.py
+++ server/database.py
@@ -1,21 +1,57 @@
-from sqlalchemy import create_engine, Column, Integer, String, Identity,MetaData, Sequence, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Identity,MetaData, Sequence, ForeignKey, Date
 from sqlalchemy.ext.declarative import declarative_base
-
+import os
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+load_dotenv()
+print(os.environ.get('PORT'))
 # Create a sqlite engine instance
-engine = create_engine("sqlite:///todooo.db")
-
+engine = create_engine(f"postgresql+psycopg2://{os.environ.get('USER')}:{os.environ.get('PASSWORD')}@{os.environ.get('HOST')}:{os.environ.get('PORT')}/{os.environ.get('DATABASE')}")
+Session = sessionmaker(bind=engine)
+print("ok")
 # Create a DeclarativeMeta instance
 Base = declarative_base()
 
 # Define To Do class inheriting from Base
 class Player(Base):
     __tablename__ = 'player'
-    id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
-    name = Column(String(256))
-    surnname = Column(String(256))
-    team = Column(Integer,ForeignKey("team.id"))
+    id_player = Column(Integer, Sequence("player_id_seq"), primary_key=True,nullable=False)
+    name = Column(String(256),nullable=False)
+    surnname = Column(String(256),nullable=False)
+    birth_data = Column(Date,nullable=False)
+    id_team = Column(Integer,ForeignKey("team.id_team"),nullable=False)
 
 class Team(Base):
+    __tablename__ = 'team'
+    id_team = Column(Integer, Sequence("team_id_seq"), primary_key=True,nullable=False)
+    name = Column(String(256),nullable=False)
+    id_league = Column(String(256),ForeignKey("league.id_league"),nullable=False)
 
+class League(Base):
+    __tablename__ = 'league'
+    id_league = Column(Integer, Sequence("league_id_seq"), primary_key=True,nullable=False)
+    name = Column(String(256),nullable=False)
+    country = Column(String(256),nullable=False)
+    level = Column(Integer,nullable=False)
+    id_inten_salary_grid = Column(Integer,ForeignKey("league.id"),nullable=False)
+    professional_minimum_wage = Column(Integer,nullable=False)
 
-class League(Base)
+class Contract(Base):
+    __tablename__ = 'contract'
+    id_contract = Column(Integer, Sequence("contract_id_seq"), primary_key=True,nullable=False)
+    id_player = Column(String(256),ForeignKey("player.id_player"),nullable=False)
+    id_team = Column(Integer,ForeignKey("team.id_team"))
+    country = Column(String(256),nullable=False)
+    date_start = Column(Date,nullable=False)
+    date_end = Column(Date,nullable=False)
+    total_salary = Column(Integer,nullable=False)
+    type_contract = Column(String(256),nullable=False)
+
+class InternSalaryGrid(Base):
+    __tablename__ = 'intern_salary_grid'
+    id_intern_salary_grid = Column(Integer, Sequence("id_intern_salary_grid_id_seq"), primary_key=True,nullable=False)
+    daily_salary_first_year = Column(Integer,nullable=False)
+    daily_salary_second_year = Column(Integer,nullable=False)
+    daily_salary_third_year = Column(Integer,nullable=False)
+print("ok2")
+Base.metadata.create_all(engine)
